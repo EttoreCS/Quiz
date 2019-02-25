93c36f35807f97b6f4adca6c18ba457f4f24f30e
Ettore Senatore <47864270+EttoreCS@users.noreply.github.com>
2019-02-25 10:29:47 -0800

    Add files via upload

--- Quiz1/FizzBuzz.java
+++ Quiz1/FizzBuzz.java
@@ -1,17 +1,20 @@
 package Quiz1;
 
-public class FizzBuzz{
-    public static void main(String[] args){
-        for(int i=1; i<=100; i++){
-            if(i%3==0 && i%5==0){
-                System.out.println("FizzBuzz");
-            }else if(i%3==0){
-                System.out.println("Fizz");
-            }else if(i%5==0){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(i);
-            }
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int number = 100;
+
+        for (int i = 1; i < 100; i++) {
+            if (i%15==0)
+                System.out.println("FizzBuzz" +" ");
+            else if (i%5==0)
+                System.out.println("Buzz" +" ");
+            else if (i%3==0)
+                System.out.println("Fizz" +" ");
+            if (i%20==0)
+                System.out.println(" ");
+            else
+                System.out.println(i +" ");
         }
     }
 }
